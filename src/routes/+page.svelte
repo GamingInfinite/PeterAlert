7d5fe57a1f401fcdf8a718ecf16b81841e309cb0
VoidBaroness <52724070+GamingInfinite@users.noreply.github.com>
2023-10-01 17:46:26 -0700

    voice recog shutdown
    I got paid $100 for this

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,43 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api";
+  import { onMount } from "svelte";
+
+  import peter from "$lib/peter.png"
+
+  let recognition;
+  let count = 0;
+
+  let griffins = ["Peter", "Meg", "Lois", "Stewie", "Brian", "Chris"];
+  let symbols = /[,.]/i;
+
+  onMount(() => {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    recognition = new SpeechRecognition();
+    recognition.continuous = true;
+    recognition.onresult = function (event) {
+      let transcript: string = event.results[count][0].transcript;
+      count++;
+      let strings = transcript.split(" ");
+      for (let i = 0; i < strings.length; i++) {
+        const element = strings[i].replace(symbols, "");
+        if (griffins.includes(element)) {
+          shutdown();
+        }
+      }
+    };
+    recognition.start();
+  });
+
+  async function shutdown() {
+    await invoke("js_shutdown");
+  }
+</script>
+
 <svelte:head>
-    <title>TableTalk</title>
+  <title>TableTalk</title>
 </svelte:head>
 
-<button class="btn btn-accent">
-Wacky
-</button>
+<div class="flex flex-row justify-center">
+  <img src="{peter}">
+</div>
